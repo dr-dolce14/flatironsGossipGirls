cfa371ee89e3712df406c5ed95af6f4ef3bd6f6c
MaleehaBhuiyan <maleehabhuiyan@baruchmail.cuny.edu>
2020-07-28 18:24:39 -0400

    finished rendering stuff

--- src/index.js
+++ src/index.js
@@ -17,23 +17,61 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function renderCeleb(celebrity) {
        const li = document.createElement('li')
-       li.className = "flex-item"
+       li.className = "box"
        li.innerText = celebrity.name
        li.dataset.name = celebrity.name
        li.setAttribute('src', `${celebrity.image}`) 
        li.dataset.occupation = celebrity.occupation
        celebUl.append(li)
+
+       celebrity.posts.forEach( post => renderPost(post))
     }
 
-    document.addEventListener('click', function(e) {
-        if(e.target.className === 'flex-item') {
-            console.log(e.target)
-            let text = e.target.innerText 
-            
-           text.fontcolor("green")
-        }
+    function renderPost(post){
+        const newsFeed = document.getElementById("news-feed")
+        newsFeed.innerHTML += `
+        <h1>${post.headline}</h1>
+        <img src= ${post.image}>
+        <br/>
+        <br/>
+        <div class="caption"> ${post.content}</div>
+        <br/>
+        <div id="reaction-table">
+            <ul id="reaction-list">
+                <li>😍 
+                    <span class="like-span">9</span>
+                </li>
+                <li>😱
+                    <span class="like-span">3</span>
+                </li>
+                <li>😢
+                    <span class="like-span">5</span>
+                </li>
+                <li>😡
+                    <span class="like-span">2</span>
+                </li>
+                <li>💩
+                    <span class="like-span">0</span>
+                </li>
+            </ul>
+        </div>
+        `
+    }
 
-    })
+    const clickHandler = () => {
+        document.addEventListener("click", function(e){
+            if(e.target.matches(".box")){
+                //e.target.style.color = 'red'
+                fetch()
+            }
+        })
+    }
+
+    // document.addEventListener('mouseover', function(e) {
+    //     if(e.target.matches('.flex-item')) {
+    //         e.target.innerText = "Hi"
+    //     }
+    // })
 
     // document.addEventListener('mouseleave', function(e) {
     //     if(e.target.matches('.flex-item')) {
@@ -41,7 +79,16 @@ document.addEventListener('DOMContentLoaded', () => {
     //     }
     // })
 
+    /*
+
+    breedUl.addEventListener("click", function(e) {
+         e.target.style.color = 'red'
+    });
+
+    */
 
 
-fetchCelebs()
+
+    fetchCelebs()
+    clickHandler()
 }) // end of DOM Content Loaded
