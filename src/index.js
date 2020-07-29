a021f42c5cab27204b21f4f2d71bf5d558f146ba
dr-dolce14 <dolcemascop1@gmail.com>
2020-07-29 13:55:22 -0400

    paola end at 2pm wed

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ console.log("IS THIS THING ON???")
 
 document.addEventListener('DOMContentLoaded', () => {
     CELEB_URL = "http://localhost:3000/celebrities"
+    POST_URL = "http://localhost:3000/posts"
     const celebUl = document.querySelector('.flex-container')
 
 
@@ -22,14 +23,18 @@ document.addEventListener('DOMContentLoaded', () => {
        li.dataset.name = celebrity.name
        li.setAttribute('src', `${celebrity.image}`) 
        li.dataset.occupation = celebrity.occupation
+       li.dataset.id = celebrity.id
        celebUl.append(li)
-
-       celebrity.posts.forEach( post => renderPost(post))
-    }
+       
+    //    celebrity.posts.forEach( post => renderPost(post))
+    } // <- take this out of here and put it in our clickhandler if statement 
 
     function renderPost(post){
+        // console.log(post)
         const newsFeed = document.getElementById("news-feed")
+        // newsFeed.innerHTML = ''
         newsFeed.innerHTML += `
+        <div class="post-div" id=${post.celebrity_id}>
         <h1>${post.headline}</h1>
         <img src= ${post.image}>
         <br/>
@@ -55,17 +60,40 @@ document.addEventListener('DOMContentLoaded', () => {
                 </li>
             </ul>
         </div>
+    </div
         `
+        // console.log(post)
     }
+
+    
 
     const clickHandler = () => {
         document.addEventListener("click", function(e){
             if(e.target.matches(".box")){
-                //e.target.style.color = 'red'
-                fetch()
+                const thisCeleb = e.target
+                thisCeleb.style.color = 'red'
+                fetch(POST_URL)
+                .then(res => res.json())
+                .then(celebrityPost => {
+                    celebrityPost.forEach 
+                // const postDiv = document.querySelectorAll('.post-div')
+                // console.log(postDiv)
+                // postDiv.forEach(post => {
+                //     console.log(post)
+                //     if (thisCeleb.dataset.id === post.id) {
+                //         renderPost(post)
+                //         console.log(thisCeleb)
+    
+                //     } 
+                // })
+             
             }
         })
     }
+
+    // make a separate fetch request to the specific id of the celebrity we're clicking
+
+
 
     // document.addEventListener('mouseover', function(e) {
     //     if(e.target.matches('.flex-item')) {
